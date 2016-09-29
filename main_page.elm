834c8e2963c0297d9502663c39b04f4269f587c1
Ashton Gabrielsen <ashton.gabrielsen@i28332-IS3.local>
2016-09-29 14:23:12 -0600

    moar code clean

--- main_page.elm
+++ main_page.elm
@@ -45,99 +45,115 @@ iconEmail = Icon.i "email"
 
 mainGrid : (Html Msg)
 mainGrid =
-  grid [ Options.css "width" "100%"]
-       [ cell [ size All 6 ]
-              [ Options.div [ Options.css "border" "3px solid blue" ]
-                            [ h1 [ style [ ( "text-align", "center" ) ] ] [ text "Metrics" ]
-                            , Options.div []
-                                          [ Options.img 
-                                            [ Options.css "width" "50%" ]
-                                            [ Html.Attributes.src "/assets/images/dial.jpg" ]
-                                          , Options.img
-                                            [ Options.css "width" "50%" ]
-                                            [ Html.Attributes.src "/assets/images/bar.png" ]
-                                          ]
-                            , Options.div []
-                                          [ Options.img 
-                                            [ Options.css "width" "50%" ]
-                                            [ Html.Attributes.src "/assets/images/dial.jpg" ]
-                                          , Options.img
-                                            [ Options.css "width" "50%" ]
-                                            [ Html.Attributes.src "/assets/images/bar.png" ]
-                                          ]
-                            , Options.div []
-                                          [ Options.img 
-                                            [ Options.css "width" "50%" ]
-                                            [ Html.Attributes.src "/assets/images/dial.jpg" ]
-                                          , Options.img
-                                            [ Options.css "width" "50%" ]
-                                            [ Html.Attributes.src "/assets/images/bar.png" ]
-                                          ]
-                            ]
-              ]
-       , cell [ size All 6 ]
-              [ Options.div [ Color.background (Color.color Color.Teal Color.S50 ) ]
-              , Options.css "min-height" "70%" ]
-                           [  ]
-              ]
-       ]
+  grid
+    [ Options.css "width" "100%"]
+    [ cell
+      [ size All 6 ]
+      [ Options.div
+        [ Options.css "border" "3px solid blue" ]
+        [ h1
+          [ style [ ("text-align", "center" ) ] ]
+          [ text "Metrics" ]
+        , Options.div
+          []
+          [ Options.img
+            [ Options.css "width" "50%" ]
+            [ Html.Attributes.src "/assets/images/dial.jpg" ]
+          , Options.img
+            [ Options.css "width" "50%" ]
+            [ Html.Attributes.src "/assets/images/bar.png" ]
+          ]
+        , Options.div
+          []
+          [ Options.img
+            [ Options.css "width" "50%" ]
+            [ Html.Attributes.src "/assets/images/dial.jpg" ]
+          , Options.img
+            [ Options.css "width" "50%" ]
+            [ Html.Attributes.src "/assets/images/bar.png" ]
+          ]
+        , Options.div
+          []
+          [ Options.img
+            [ Options.css "width" "50%" ]
+            [ Html.Attributes.src "/assets/images/dial.jpg" ]
+          , Options.img
+            [ Options.css "width" "50%" ]
+            [ Html.Attributes.src "/assets/images/bar.png" ]
+          ]
+        ]
+      ]
+    , cell
+      [ size All 6 ]
+      [ Options.div
+        [ Color.background ( Color.color Color.Teal Color.S50)
+        , Options.css "min-height" "70%" ]
+        []
+      ]
+    ]
 
 view : Model -> Html Msg
 view model =
-    Layout.render Mdl
-        model.mdl
-        [ Layout.fixedHeader
-        ]
-        { header = [ viewHeader ]
-        , drawer = []
-        , tabs = ( [], [] )
-        , main = [ viewBody model ]
-        }
+  Layout.render Mdl
+    model.mdl
+      [ Layout.fixedHeader ]
+      { header = [ viewHeader ]
+      , drawer = []
+      , tabs = ( [], [] )
+      , main = [ viewBody model ]
+      }
 
 viewHeader : Html Msg
 viewHeader =
-    Options.div [ Color.background (Color.color Color.Teal Color.S400)
- ]
-    [ h1  [ style [ ( "padding", "2rem" ) ] ] 
-          [ text "BlitzDerektor" ]
-    , viewStepper
+  Options.div
+    [ Color.background (Color.color Color.Teal Color.S400) ]
+    [ h1
+      [ style [ ( "padding", "2rem" ) ] ] 
+      [ text "BlitzDerektor" ]
+      , viewStepper
     ]
 
 viewStepper : Html Msg
 viewStepper = 
-    Layout.row [ Options.css "display" "flex" 
-                , Color.background (Color.color Color.Teal Color.S200) ]
-
-                [ Options.div [ Options.css "width" "15%" ] 
-                              [ iconEmail
-                              , text "Step 1" ]
-                , Options.div [ Options.css "width" "15%" ] 
-                              [ iconEmail
-                              , text "Step 2" ]
-                , Options.div [ Options.css "width" "15%" ] 
-                              [ iconEmail
-                              , text "Step 3" ]
-                , Options.div [ Options.css "width" "15%" ] 
-                              [ iconEmail
-                              , text "Step 4" ]
-                , Options.div [ Options.css "width" "15%" ] 
-                              [ iconEmail
-                              , text "Step 5" ]
-                , Options.div [ Options.css "width" "15%" ] 
-                              [ iconEmail
-                              , text "Step 6" ]
-                ]
+  Layout.row
+    [ Options.css "display" "flex" 
+    , Color.background (Color.color Color.Teal Color.S200) ]
+    [ Options.div
+      [ Options.css "width" "15%" ] 
+      [ iconEmail
+      , text "Step 1" ]
+    , Options.div
+      [ Options.css "width" "15%" ] 
+      [ iconEmail
+      , text "Step 2" ]
+    , Options.div
+      [ Options.css "width" "15%" ] 
+      [ iconEmail
+      , text "Step 3" ]
+    , Options.div
+      [ Options.css "width" "15%" ] 
+      [ iconEmail
+      , text "Step 4" ]
+    , Options.div
+      [ Options.css "width" "15%" ] 
+      [ iconEmail
+      , text "Step 5" ]
+    , Options.div
+      [ Options.css "width" "15%" ] 
+      [ iconEmail
+      , text "Step 6" ]
+    ]
 
 viewBody : Model -> Html Msg
 viewBody model =
-         mainGrid
-        |> Material.Scheme.top
+  mainGrid
+    |> Material.Scheme.top
 
 main : Program Never
 main =
-    App.program
-        { init = ( model, Cmd.none )
-        , view = view
-        , subscriptions = always Sub.none
-        , update = update
-        }
+  App.program
+    { init = ( model, Cmd.none )
+    , view = view
+    , subscriptions = always Sub.none
+    , update = update
+    }
