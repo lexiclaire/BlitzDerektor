5258ed533ac93b0d71bcc886f0bc8740990e3bd6
Gina Vasiloff <ginavasiloff@gmail.com>
2016-11-10 10:58:00 -0700

    Created scaffolding for the data

--- src/Derektor/Data.elm
+++ src/Derektor/Data.elm
@@ -1,9 +1,12 @@
 module Derektor.Data exposing (..)
 
+import Date
+import Set exposing (Set)
+
+import Uuid exposing (..)
+
 import Material
 import Material.Color as Color
-
-import Set exposing (Set)
 
 type alias Model =
   { mdl : Material.Model
@@ -19,3 +22,46 @@ type Msg
   | SelectJobsTab Int
   | SelectStepperTab Int
   | Toggle String
+
+type alias Job =
+  { uuid : Uuid.Uuid
+  , name : String
+  , stp : Int
+  , locked : Bool
+  , lastEdited : Date.Date
+  , template : Template
+  , query : Query
+  , review : Review
+  , schedule : Schedule
+  , recipients : List Recipient
+  }
+
+type alias JobSummary =
+  { uuid : Uuid.Uuid
+  , name : String
+  , stp : Int
+  , locked : Bool
+  , lastEdited : Date.Date
+  }
+
+type alias Template = String
+
+type alias TemplateSummary = String
+
+type alias Query = String
+
+type alias QuerySummary = String
+
+type alias Review = String
+
+type alias Schedule = String
+
+type alias Recipient = String
+
+type alias Jobs = List JobSummary
+
+type alias Templates = List TemplateSummary
+
+type alias Queries = List QuerySummary
+
+type alias Schedules = List Schedule
